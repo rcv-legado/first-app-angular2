c2acf6281137ae40837a73eb8c7ff4df62297ad1
Rafael Guinho Monteiro <rafaelguinho@outlook.com>
2015-12-29 16:43:53 -0300

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'templates/my-app.html'
+})
+export class AppComponent { 
+    public name = "";
+}
